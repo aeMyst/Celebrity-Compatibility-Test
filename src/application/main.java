f7b494106900e5b8b3fb0c5bf99818f74926c8cf
Peter Tran <panpetertran123@gmail.com>
2022-11-03 14:35:49 -0600

    Project Setup + Welcome Scene

--- src/application/main.java
+++ src/application/main.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
  
+//change
 public class main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
