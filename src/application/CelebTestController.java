6242c3b81833896fdff15e37e51cfe85d1190a41
JuliiRuta <julii.ruta@ucalgary.ca>
2022-11-04 10:30:20 -0600

    Questions scene done

--- src/application/CelebTestController.java
+++ src/application/CelebTestController.java
@@ -17,22 +17,12 @@ public class CelebTestController {
 	@FXML
     private Button startButton;
 	
-	@FXML
-	private Slider SpontaneousSlider;
-	
-	@FXML
-	private ChoiceBox<?> ZodiacSignChoiceBox;
-	
-	@FXML
-	private Slider IntroExtroSlider;
-	
-
     @FXML
     void changeToQuestions(ActionEvent event) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			VBox root = loader.load(new FileInputStream("src/application/QuestionView.fxml"));
-			Scene secondaryScene = new Scene(root,1024,768);
+			Scene secondaryScene = new Scene(root,300,675);
 			
 			applicationStage.setScene(secondaryScene);
 			applicationStage.setTitle("Quiz Questions");
@@ -41,6 +31,103 @@ public class CelebTestController {
 			e.printStackTrace();
 		}
     }
+	
+	//food buttons
+	@FXML 
+	private Button PizzaFoodButton;
+	
+	@FXML 
+	private Button PastaFoodButton;
+	
+	@FXML 
+	private Button SushiFoodButton;
+	
+	@FXML 
+	private Button IceCreamFoodButton;
+	
+	@FXML
+	void setFoodAnswer (ActionEvent event) {
+		//enter code that will match users answer with canidate's answer and increase compatibility %
+	}
+	
+	//season buttons
+	@FXML 
+	private Button FallSeasonButton;
+	
+	@FXML 
+	private Button WinterSeasonButton;
+	
+	@FXML 
+	private Button SpringSeasonButton;
+	
+	@FXML 
+	private Button SummerSeasonButton;
+	
+	@FXML
+	void setSeasonAnswer (ActionEvent event) {
+		//enter code that will match users answer with canidate's answer and increase compatibility %
+		//change text colour or something of whatever button is pressed to show what answer 
+		//has been selected
+	}
+	
+	//colour buttons
+	@FXML 
+	private Button RedColourButton;
+	
+	@FXML 
+	private Button GreenColourButton;
+	
+	@FXML 
+	private Button BlueColourButton;
+	
+	@FXML 
+	private Button YellowColourButton;
+	
+	@FXML
+	void setColourAnswer (ActionEvent event) {
+		//enter code that will match users answer with canidate's answer and increase compatibility %
+	}
+	
+	//music buttons
+	@FXML 
+	private Button RapMusicButton;
+	 
+	@FXML 
+	private Button PopMusicButton;
+	
+	@FXML 
+	private Button IndieMusicButton;
+	
+	@FXML 
+	private Button ClassicalMusicButton;
+	
+	@FXML
+	void setMusicAnswer (ActionEvent event) {
+		//enter code that will match users answer with canidate's answer and increase compatibility %
+	}
+	
+	//animal buttons
+	@FXML 
+	private Button CatAnimalButton;
+	
+	@FXML 
+	private Button DogAnimalButton;
+	
+	@FXML
+	void setAnimalAnswer (ActionEvent event) {
+		//enter code that will match users answer with canidate's answer and increase compatibility %
+	}
+	
+	//other components of questions
+	@FXML
+	private Slider SpontaneousSlider;
+	
+	@FXML
+	private ChoiceBox<String> ZodiacSignChoiceBox;
+	
+	@FXML
+	private Slider IntroExtroSlider;
+	
 
 
 	@FXML
