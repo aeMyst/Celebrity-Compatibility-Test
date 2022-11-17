b80ee7f3fe36f19f71ee85b1392e8ebe685b6f94
Peter Tran <panpetertran123@gmail.com>
2022-11-17 00:30:26 -0700

    verified User input for names, and letter matching to user input + updated fxml to have fx:id

--- src/application/CelebTestController.java
+++ src/application/CelebTestController.java
@@ -1,6 +1,10 @@
 package application;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,11 +13,25 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class CelebTestController {
 	Stage applicationStage;
+	HashMap<String, Double> celebDictionary = new  HashMap<String, Double>();
+	
+	@FXML
+	private TextField nameTextField;
+	
+	@FXML
+	private Label nameErrorLabel;
+	
+	@FXML
+	private TextField ageTextField;
+	
+	@FXML
+	private Label ageErrorLabel;
     
 	@FXML
     private Button startButton;
@@ -23,7 +41,7 @@ public class CelebTestController {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			VBox root = loader.load(new FileInputStream("src/application/QuestionView.fxml"));
-			Scene secondaryScene = new Scene(root,300,675);
+			Scene secondaryScene = new Scene(root,600,675);
 			
 			applicationStage.setScene(secondaryScene);
 			applicationStage.setTitle("Quiz Questions");
@@ -133,29 +151,106 @@ public class CelebTestController {
 	@FXML
 	private Label DisplayCompatibilityScoreLabel;
 	
-	
-
-
 	@FXML
 	private Button DoneButton;
+	
+	@FXML
+	String verifyNames(String personName) {
+		// initialization of variables
+		boolean validName = true;
+		char invalidChar = Character.MIN_VALUE;
+		
+		for (char c : personName.toCharArray()) {
+   	  		if  ((c >= '!' && c <= '@') || (c >= '[' && c <= '`') || (c >= '{' && c <= '~') ) {
+   	  			validName = false;
+   	  			invalidChar = c;
+   	  		} 
+   	  		if (!(validName)) {
+   	  			nameErrorLabel.setText("Do not use " + invalidChar + " in your name. Please enter a valid name.");
+   	  		} 
+	    }
+		if (validName) {
+			return personName;
+		} else {
+			return "";
+		}
+	}
+	
+	void calculateNames(String personName) {
+		ArrayList<Integer> countList = new ArrayList<Integer>();
+		int charCountJB = 0;
+		int charCountKW = 0;
+		int charCountJL = 0;
+		int charCountTS = 0;
+		
+		String jb = "Jennifer Lopez";
+		String kw = "Kanye West";		
+		String jl = "Jennifer Lopez";
+		String ts = "Taylor Swift";
+		
+		personName = personName.toLowerCase();
+		
+		// loop for jb
+		for (int i = 0; i < personName.length(); i++) { 
+			for (int p = 0; p < jb.length(); p++) {
+				if (personName.charAt(i) == jb.charAt(p)) {
+					charCountJB += 1; 
+				}	
+			}	
+		}
+		System.out.print("Justin Bieber " + charCountJB + '\n');
+		
+		// loop for kw
+		for (int i = 0; i < personName.length(); i++) { 
+			for (int p = 0; p < kw.length(); p++) {
+				if (personName.charAt(i) == kw.charAt(p)) {
+					charCountKW += 1; 
+				}	
+			}	
+		}
+		System.out.print("Kanye West " + charCountKW + '\n');
+		
+		// loop for jl
+		for (int i = 0; i < personName.length(); i++) { 
+			for (int p = 0; p < jl.length(); p++) {
+				if (personName.charAt(i) == jl.charAt(p)) {
+					charCountJL += 1; 
+				}	
+			}	
+		}
+		System.out.print("Jennifer Lopez " + charCountJL + '\n');
+		
+		// loop for ts
+		for (int i = 0; i < personName.length(); i++) { 
+			for (int p = 0; p < ts.length(); p++) {
+				if (personName.charAt(i) == ts.charAt(p)) {
+					charCountTS += 1; 
+				}	
+			}	
+		}
+		System.out.print("Taylor Swift " + charCountTS + '\n');
+		
+		countList.add(charCountJB);
+		countList.add(charCountKW);
+		countList.add(charCountJL);
+		countList.add(charCountTS);
+		
+		countList.sort(Collections.reverseOrder());
+		System.out.print("My counts from Highest to Lowest " + countList);
+		
+	}
 	
 	//this method does not work for some reason, please check
 	@FXML 
 	void changeToFinal(ActionEvent event) {
-		try {
-			
-			FXMLLoader loader = new FXMLLoader();
-			VBox root = loader.load(new FileInputStream("src/application/FinalView.fxml"));
-			Scene finalScene = new Scene(root,1024,768);
-			
-			applicationStage.setScene(finalScene);
-			applicationStage.setTitle("Results");
-			applicationStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		
+		String personName = nameTextField.getText();
+		String verifiedPersonName = verifyNames(personName);
+		if (verifiedPersonName.equals("")) {
+			System.out.print('\n' + verifiedPersonName + '\n');
+		} else {
+			System.out.print('\n' + verifiedPersonName + '\n');
+			calculateNames(verifiedPersonName);
+		}		
 	}
 }
-
-
