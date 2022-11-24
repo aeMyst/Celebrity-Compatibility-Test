8e73a48058f5e3aed7eeb2af896d41411d7f3441
Peter Tran <panpetertran123@gmail.com>
2022-11-24 12:16:40 -0700

    Adding piechart to data collection

--- src/application/FinalViewController.java
+++ src/application/FinalViewController.java
@@ -2,12 +2,15 @@ package application;
 
 import java.io.FileInputStream;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -17,10 +20,13 @@ public class FinalViewController{
 	@FXML 
 	private BarChart<?, ?> barChart;
 	
+	@FXML
+	private PieChart pieChart; 
+	
 	public void createBarGraph(double jbData, double jloData, double kwData, double tsData) {
 		XYChart.Series series = new XYChart.Series<>();
 		
-		series.setName("Celebrity Compatibility Results");
+		series.setName("Celebrity Compatibility Results in %");
 
 		series.getData().add(new XYChart.Data<>( "Justin Bieber",  jbData));
 		series.getData().add(new XYChart.Data<>( "Jennifer Lopez",  jloData));
@@ -31,10 +37,16 @@ public class FinalViewController{
 		barChart.setLegendVisible(false);
 		
 
+	}
+
+	public void createPieChart(double jbData, double jloData, double kwData, double tsData)	{
+		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+				new PieChart.Data("Justin Bieber" , jbData),
+				new PieChart.Data("Jennnifer Lopez" , jloData),
+				new PieChart.Data("Kanye West" , kwData),
+				new PieChart.Data("Taylor Swift" , tsData));
 		
-		//root.getChildren().add(barChart);
-		
-		
-		
+		pieChart.setData(pieChartData);
+		pieChart.setTitle("Celebrity Compatibility in % on PieChart");
 	}
 }
