cd6759630df9423736b0dc26d7a412d121035854
JaimieMarchuk <CS219-user@10.0.0.66>
2022-12-08 18:00:22 -0700

    Added JavaDoc to the food button.

--- src/application/CelebTestController.java
+++ src/application/CelebTestController.java
@@ -149,7 +149,11 @@ public class CelebTestController {
 
     }
 	
-	// method to call Question class for food buttons
+	/**
+	 * Method that creates instances of the Question class to add the
+	 * calculated compatibility from the food button to the celebrities
+	 * lists. 
+	 */
 	@FXML
 	void setFoodAnswer (ActionEvent event) {
 
